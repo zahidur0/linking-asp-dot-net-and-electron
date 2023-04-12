5b52a4ccc624a5b1614c25fccabea584bab0f45e
Zahidur <zahidur01111010@gmail.com>
2023-04-12 21:20:38 +0100

    Initialise ASP.NET Core Web API project

---
+++ WeatherForecast.cs
@@ -0,0 +1,12 @@
+namespace linking_asp_dot_net_and_electron;
+
+public class WeatherForecast
+{
+    public DateTime Date { get; set; }
+
+    public int TemperatureC { get; set; }
+
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+    public string? Summary { get; set; }
+}
